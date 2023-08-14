ef08f783bee246403bcc03b4a635f424f8cdefcf
ireero <78822225+ireero@users.noreply.github.com>
2023-08-13 23:34:33 -0300

    Otimizando código.

--- lib/models/check_box_list.dart
+++ lib/models/check_box_list.dart
@@ -13,42 +13,38 @@ class ListCheckBox extends StatefulWidget {
 }
 
 class _ListCheckBoxState extends State<ListCheckBox> {
+  void mudaBool(int index) {
+    setState(() {
+      widget.lista_checks[index] = !widget.lista_checks[index];
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Row(mainAxisAlignment: MainAxisAlignment.spaceAround, children: [
       Checkbox(
         activeColor: Colors.deepOrange,
-        onChanged: (value) => setState(() {
-          widget.lista_checks[0] = !widget.lista_checks[0];
-        }),
+        onChanged: (value) => mudaBool(0),
         value: widget.lista_checks[0],
       ),
       Checkbox(
         activeColor: Colors.deepOrange,
-        onChanged: (value) => setState(() {
-          widget.lista_checks[1] = !widget.lista_checks[1];
-        }),
+        onChanged: (value) => mudaBool(1),
         value: widget.lista_checks[1],
       ),
       Checkbox(
         activeColor: Colors.deepOrange,
-        onChanged: (value) => setState(() {
-          widget.lista_checks[2] = !widget.lista_checks[2];
-        }),
+        onChanged: (value) => mudaBool(2),
         value: widget.lista_checks[2],
       ),
       Checkbox(
         activeColor: Colors.deepOrange,
-        onChanged: (value) => setState(() {
-          widget.lista_checks[3] = !widget.lista_checks[3];
-        }),
+        onChanged: (value) => mudaBool(3),
         value: widget.lista_checks[3],
       ),
       Checkbox(
         activeColor: Colors.deepOrange,
-        onChanged: (value) => setState(() {
-          widget.lista_checks[4] = !widget.lista_checks[4];
-        }),
+        onChanged: (value) => mudaBool(4),
         value: widget.lista_checks[4],
       )
     ]);
